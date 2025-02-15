17ebe501b5df6f2e0f68c5394633c362d3c2606e
Herisson Nobre <herisson.carvalho96@gmail.com>
2025-02-15 09:31:56 -0300

    feat: login page

---
+++ win-app/ViewModels/LoginViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace win_app.ViewModels;
+
+public class LoginViewModel : INotifyPropertyChanged
+{
+    private string email;
+    private string password;
+
+    public string Email
+    {
+        get => email;
+        set
+        {
+            if (email == value) return;
+            email = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Password
+    {
+        get => password;
+        set
+        {
+            if (password == value) return;
+            password = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ win-app/Views/LoginPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace win_app.Views;
+
+public partial class LoginPage : Page
+{
+    public LoginPage()
+    {
+        InitializeComponent();
+    }
+
+    private void LoginButton_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        string email = EmailTextBox.Text;
+        string password = PasswordBox.Password;
+
+        if (email == "" || password == "")
+        {
+            MessageBox.Show("Please, enter your email and password.");
+        }
+        else
+        {
+            MessageBox.Show("You entered your email and password.");
+        }
+    }
+}
